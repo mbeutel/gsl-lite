mbeutel/gsl-lite

commit 4345081b062b2c68eda08b9832b6938f9dee7d91

More `not_null<>` improvements, some general improvements (#202)
* Add macros `gsl_noreturn`, `gsl_nodiscard`, `gsl_deprecated()` (https://github.com/martinmoene/gsl-lite/commit/66f62f8ddf77e218771aa2fc0e195e922c46a144)

* CMake package now installs only <gsl/gsl-lite.hpp> by default (https://github.com/martinmoene/gsl-lite/commit/ff9bd3411adac51040db0b8b82863b0c22104c39)

* Fix implicit conversion operators and operators `->` and `*` of `not_null<>` (https://github.com/martinmoene/gsl-lite/commit/4c060db5100c873020142ecb8d11305c499f033f, https://github.com/martinmoene/gsl-lite/commit/dcc811e1c2829706152d39fce3dc802abcfd2b0b, https://github.com/martinmoene/gsl-lite/commit/9122aa558e3bfdc57269787ceab1cd8e34f12c6b, https://github.com/martinmoene/gsl-lite/commit/ae5ea3e136060c5aaa5d88214019cb2186aa9066)

* `not_null<>` comparison operators are now transparent and use SFINAE (https://github.com/martinmoene/gsl-lite/commit/c67b5373090781528b49531195aa08e23d33641c)

* Add configuration option `gsl_CONFIG_NOT_NULL_TRANSPARENT_GET` (https://github.com/martinmoene/gsl-lite/commit/a84560ef71816e4b44d5d8a18b34fbf071e23ee6)

* Add deduction guides for `not_null<>` (https://github.com/martinmoene/gsl-lite/commit/7a6278305c8ff644736c5d798bc5591dc15e5051)

* Add `make_not_null()` (https://github.com/martinmoene/gsl-lite/commit/042d4749891ccac2deca844d9d331b44c7b47e2d)

test/not_null.t.cpp
@@ -119,7 +119,27 @@ CASE( "not_null<>: Allows to construct from a non-null underlying pointer (raw p
 
     not_null< int* > p( &i );
 
-    EXPECT( p.get() == &i );
+    EXPECT( p == &i );
+}
+
+CASE( "not_null<>: Allows to construct from a non-null underlying pointer (raw pointer) with make_not_null()" )
+{
+    int i = 12;
+
+    not_null< int* > p = make_not_null( &i );
+
+    EXPECT( p == &i );
+}
+
+CASE( "not_null<>: Allows to construct from a non-null underlying pointer (raw pointer) with deduction guide" )
+{
+#if gsl_HAVE( DEDUCTION_GUIDES )
+    int i = 12;
+
+    not_null p( &i );
+
+    EXPECT( p == &i );
+#endif
 }
 
 CASE( "not_null<>: Allows to construct a const pointer from a non-null underlying pointer (raw pointer)" )
@@ -128,7 +148,7 @@ CASE( "not_null<>: Allows to construct a const pointer from a non-null underlyin
 
     not_null< const int* > p( &i );
 
-    EXPECT( p.get() == &i );
+    EXPECT( p == &i );
 }
 
 CASE( "not_null<>: Allows to construct from a non-null related pointer (raw pointer)" )
@@ -136,7 +156,7 @@ CASE( "not_null<>: Allows to construct from a non-null related pointer (raw poin
     MyDerived derived;
     not_null< MyBase* > p( &derived );
 
-    EXPECT( p.get() == &derived );
+    EXPECT( p == &derived );
 }
 
 CASE( "not_null<>: Allows to construct a const pointer from a non-null related pointer (raw pointer)" )
@@ -144,7 +164,7 @@ CASE( "not_null<>: Allows to construct a const pointer from a non-null related p
     MyDerived derived;
     not_null< const MyBase* > p( &derived );
 
-    EXPECT( p.get() == &derived );
+    EXPECT( p == &derived );
 }
 
 CASE( "not_null<>: Allows to construct from a not_null related pointer type (raw pointer)" )
@@ -251,7 +271,27 @@ CASE( "not_null<>: Allows to construct from a non-null underlying pointer (share
     shared_ptr< int > pi = make_shared< int >(12);
     not_null< shared_ptr< int > > p( pi );
 
-    EXPECT( p.get() == pi );
+    EXPECT( p == pi );
+}
+
+CASE( "not_null<>: Allows to construct from a non-null underlying pointer (shared_ptr) with make_not_null()" )
+{
+    shared_ptr< int > pi = make_shared< int >(12);
+
+    not_null< shared_ptr< int > > p = make_not_null( pi );
+
+    EXPECT( p == pi );
+}
+
+CASE( "not_null<>: Allows to construct from a non-null underlying pointer (shared_ptr) with deduction guide" )
+{
+#if gsl_HAVE( DEDUCTION_GUIDES )
+    shared_ptr< int > pi = make_shared< int >(12);
+
+    not_null p( pi );
+
+    EXPECT( p == pi );
+#endif
 }
 
 CASE( "not_null<>: Allows to construct a const pointer from a non-null underlying pointer (shared_ptr)" )
@@ -259,7 +299,7 @@ CASE( "not_null<>: Allows to construct a const pointer from a non-null underlyin
     shared_ptr< int > pi = make_shared< int >(12);
     not_null< shared_ptr< const int > > p( pi );
 
-    EXPECT( p.get() == pi );
+    EXPECT( p == pi );
 }
 
 CASE( "not_null<>: Allows to construct from a non-null related pointer (shared_ptr)" )
@@ -267,7 +307,7 @@ CASE( "not_null<>: Allows to construct from a non-null related pointer (shared_p
     shared_ptr< MyDerived > pderived = make_shared< MyDerived >();
     not_null< shared_ptr< MyBase > > p( pderived );
 
-    EXPECT( p.get() == pderived );
+    EXPECT( p == pderived );
 }
 
 CASE( "not_null<>: Allows to construct a const pointer from a non-null related pointer (shared_ptr)" )
@@ -275,7 +315,7 @@ CASE( "not_null<>: Allows to construct a const pointer from a non-null related p
     shared_ptr< MyDerived > pderived = make_shared< MyDerived >();
     not_null< shared_ptr< const MyBase > > p( pderived );
 
-    EXPECT( p.get() == pderived );
+    EXPECT( p == pderived );
 }
 
 CASE( "not_null<>: Allows to construct from a not_null related pointer type (shared_ptr)" )
@@ -401,6 +441,42 @@ CASE( "not_null<>: Allows to construct from a non-null underlying pointer (uniqu
     not_null< unique_ptr< int > > p( std::move(pi) );
 
     EXPECT( &*p == raw );
+}
+
+CASE( "not_null<>: Allows to move from a not-null pointer (unique_ptr)" )
+{
+#if gsl_HAVE( FUNCTION_REF_QUALIFIER )
+    unique_ptr< int > pi = make_unique< int >(12);
+    int* raw(pi.get());
+
+    not_null< unique_ptr< int > > p ( std::move(pi) ); // There...
+    pi = std::move(p); // ...and back again.
+
+    EXPECT_THROWS( *p );
+    EXPECT( pi.get() == raw );
+#endif
+}
+
+CASE( "not_null<>: Allows to construct from a non-null underlying pointer (unique_ptr) with make_not_null()" )
+{
+    unique_ptr< int > pi = make_unique< int >(12);
+    int* raw(pi.get());
+
+    not_null< unique_ptr< int > > p = make_not_null( std::move(pi) );
+
+    EXPECT( &*p == raw );
+}
+
+CASE( "not_null<>: Allows to construct from a non-null underlying pointer (unique_ptr) with deduction guide" )
+{
+#if gsl_HAVE( DEDUCTION_GUIDES )
+    unique_ptr< int > pi = make_unique< int >(12);
+    int* raw(pi.get());
+
+    not_null p( std::move(pi) );
+
+    EXPECT( &*p == raw );
+#endif
 }
 
 CASE( "not_null<>: Allows to construct a const pointer from a non-null underlying pointer (unique_ptr)" )
@@ -636,9 +712,9 @@ CASE( "not_null<>: Allows assignment from a non-null bare recast pointer" )
     MyDerived derived;
     not_null< MyDerived* > p( &derived );
 
-    not_null< Unrelated* > t( reinterpret_cast< Unrelated* >( p.get() ) );
+    not_null< Unrelated* > t( reinterpret_cast< Unrelated* >( &*p ) );
 
-    EXPECT( p.get() == reinterpret_cast< MyDerived* >( t.get() ) );
+    EXPECT( &*p == reinterpret_cast< MyDerived* >( &*t ) );
 }
 
 CASE( "not_null<>: Allows implicit conversion to underlying type" )
@@ -658,7 +734,7 @@ CASE( "not_null<>: Allows to construct from a non-null user-defined ref-counted 
 
     not_null<   int*>  p( rp );
 
-    EXPECT( p.get() == &i );
+    EXPECT( p == &i );
 }
 
 namespace {

CMakeLists.txt
@@ -25,7 +25,7 @@ set( package_nspace  "gsl" )
 set( package_name    "${unit_name}-lite" )
 set( package_version "${${PROJECT_NAME}_VERSION}" )
 
-message( STATUS "Project '${PROJECT_NAME}', package '${package_name}' version: '${package_version}'")
+message( STATUS "Project '${PROJECT_NAME}', package '${package_name}' version: '${package_version}'" )
 
 # Toplevel or subproject:
 
@@ -37,9 +37,11 @@ endif()
 
 # If toplevel project, enable building and performing of tests, disable building of examples:
 
-option( GSL_LITE_OPT_BUILD_TESTS    "Build and perform gsl-lite tests" ${gsl_IS_TOPLEVEL_PROJECT} )
-option( GSL_LITE_OPT_BUILD_EXAMPLES "Build gsl-lite examples" OFF )
-option( CMAKE_EXPORT_PACKAGE_REGISTRY "Export build directory to CMake user package registry" OFF )
+option( GSL_LITE_OPT_BUILD_TESTS             "Build and perform gsl-lite tests" ${gsl_IS_TOPLEVEL_PROJECT} )
+option( GSL_LITE_OPT_BUILD_EXAMPLES          "Build gsl-lite examples" OFF )
+option( CMAKE_EXPORT_PACKAGE_REGISTRY        "Export build directory to CMake user package registry" OFF )
+option( GSL_LITE_OPT_INSTALL_COMPAT_HEADER   "Install MS-GSL compatibility header <gsl/gsl>" OFF )
+option( GSL_LITE_OPT_INSTALL_LEGACY_HEADERS  "Install legacy headers <gsl.h>, <gsl.hpp>, <gsl/gsl-lite.h>" OFF )
 
 # If requested, build and perform tests, build examples:
 
@@ -134,8 +136,25 @@ install(
 )
 
 install(
-    DIRECTORY   "include/"
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
+    FILES       "include/gsl/gsl-lite.hpp"
+    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/gsl/"
 )
+if(GSL_LITE_OPT_INSTALL_COMPAT_HEADER)
+    install(
+        FILES       "include/gsl/gsl"
+        DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/gsl/"
+    )
+endif()
+if(GSL_LITE_OPT_INSTALL_LEGACY_HEADERS)
+    install(
+        FILES       "include/gsl/gsl-lite.h"
+        DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/gsl/"
+    )
+    install(
+        FILES       "include/gsl.h"
+                    "include/gsl.hpp"
+        DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/"
+    )
+endif()
 
 # end of file

example/02-span.cpp
@@ -2,6 +2,8 @@
 
 #include "gsl/gsl-lite.hpp"
 #include <iostream>
+#include <array>
+#include <vector>
 
 using namespace gsl;
 

test/assert.t.cpp
@@ -26,6 +26,15 @@ bool ensures( bool x ) { Ensures( x ); return x; }
 bool expectsAudit( bool x ) { ExpectsAudit( x ); return x; } 
 bool ensuresAudit( bool x ) { EnsuresAudit( x ); return x; }
 
+struct ConvertibleToBool
+{
+#if gsl_CPP11_OR_GREATER
+    explicit
+#endif
+    operator bool(void) const gsl_noexcept { return true; }
+};
+
+
 }
 
 CASE( "Expects(): Allows a true expression" )
@@ -78,9 +87,22 @@ CASE( "EnsuresAudit(): Terminates on a false expression in AUDIT mode" )
 
 int testAssume( int i, std::vector<int> const& v )
 {
-    // This should compile without warnings.
+    // The arguments to `__assume(x)` (MSVC) and `__builtin_assume(x)` (Clang) are never evaluated, so they cannot incur side-effects. We would like to implement
+    // `gsl_ASSUME()` in terms of these. However, Clang always emits a diagnostic if a potential side-effect is discarded, and every call to a function not annotated
+    // `__attribute__ ((pure))` or `__attribute__ ((const))` is considered a potential side-effect (e.g. the call to `v.size()` below). In many cases Clang is capable
+    // of inlining the expression and find it free of side-effects, cf. https://gcc.godbolt.org/z/ZcKfbp, but the warning is produced anyway.
+    //
+    // To avoid littering user code with warnings, we instead define `gsl_ASSUME()` in terms of `__builtin_unreachable()`. The following `gsl_ASSUME()` statement
+    // should thus compile without any warnings.
+
     gsl_ASSUME( i >= 0 && static_cast<std::size_t>(i) < v.size() );
     return v.at( static_cast<std::size_t>(i) );
+}
+
+void testConvertibleToBool()
+{
+    // `Expects()` should be compatible with explicit conversions to bool.
+    Expects( ConvertibleToBool() );
 }
 
 // end of file

test/gsl-lite.t.hpp
@@ -22,8 +22,14 @@
 // Limit C++ Core Guidelines checking to GSL Lite:
 
 #if defined(_MSC_VER) && _MSC_VER >= 1910
-# include <CppCoreCheck/warnings.h>
-# pragma warning(disable: ALL_CPPCORECHECK_WARNINGS)
+# if defined(__has_include)
+#  if __has_include(<CppCoreCheck/warnings.h>)
+#   include <CppCoreCheck/warnings.h>
+#   pragma warning(disable: ALL_CPPCORECHECK_WARNINGS)
+#  else
+#   pragma message( "Warning: <CppCoreCheck/warnings.h> not in include path; skipping" )
+#  endif
+# endif
 #endif
 
 // Compiler warning suppression for usage of lest:

commit b74283c39b9dd90b8b1e078f62cac136ccedd88a

Fix awkward issues in `not_null<>` introduced in #202; add tests
- `get()` should not need function ref qualifiers
- `get()` should return a plain pointer
- `operator->()` can safely return `T const &` in all cases
- `operator*()` should always return `element_type &`

Thanks to @petamas for feedback.

include/gsl/gsl-lite.hpp
@@ -1540,32 +1540,22 @@ public:
     }
 #endif // gsl_HAVE( RVALUE_REFERENCE )
 
-#if gsl_HAVE( FUNCTION_REF_QUALIFIER )
-# if gsl_CONFIG( NOT_NULL_TRANSPARENT_GET )
-    gsl_api gsl_constexpr14 element_type &  get() const &  { return checked_ptr().get(); }
-    gsl_api gsl_constexpr14 element_type && get() &&       { return std::move(checked_ptr()).get(); }
-# else
-    gsl_api gsl_constexpr14 get_result_t get() const { return checked_ptr(); }
-# endif
+#if gsl_CONFIG( NOT_NULL_TRANSPARENT_GET )
+    gsl_api gsl_constexpr14 element_type* get() const { return checked_ptr().get(); }
+#else
+    gsl_api gsl_constexpr14 get_result_t  get() const { return checked_ptr(); }
+#endif
 
+#if gsl_HAVE( FUNCTION_REF_QUALIFIER )
     gsl_api gsl_constexpr14 operator T() const & { return checked_ptr(); }
     gsl_api gsl_constexpr14 operator T() &&      { return std::move(checked_ptr()); }
-
-    gsl_api gsl_constexpr14 T const & operator->() const & { return checked_ptr(); }
-    gsl_api gsl_constexpr14 T &&      operator->() &&      { return std::move(checked_ptr()); }
-
-    gsl_api gsl_constexpr14 element_type &  operator*() const & { return *checked_ptr(); }
-    gsl_api gsl_constexpr14 element_type && operator*() &&      { return *std::move(checked_ptr()); }
-
 #else
-    gsl_api gsl_constexpr14 get_result_t get() const { return checked_ptr(); }
-
     gsl_api gsl_constexpr   operator get_result_t  () const   { return checked_ptr(); }
+#endif
 
-    gsl_api gsl_constexpr   get_result_t operator->() const   { return checked_ptr(); }
+    gsl_api gsl_constexpr   T const &     operator->() const   { return checked_ptr(); }
 
     gsl_api gsl_constexpr   element_type & operator*() const  { return *checked_ptr(); }
-#endif
 
 gsl_is_delete_access:
     // prevent compilation when initialized with a nullptr or literal 0:

test/not_null.t.cpp
@@ -122,6 +122,17 @@ CASE( "not_null<>: Allows to construct from a non-null underlying pointer (raw p
     EXPECT( p == &i );
 }
 
+CASE( "not_null<>: Returns underlying pointer with get() (raw pointer)" )
+{
+#if !gsl_CONFIG( NOT_NULL_TRANSPARENT_GET )
+    int i = 12;
+    not_null< int* > p( &i );
+
+    int* pg = p.get();
+    EXPECT( pg == &i );
+#endif
+}
+
 CASE( "not_null<>: Allows to construct from a non-null underlying pointer (raw pointer) with make_not_null()" )
 {
     int i = 12;
@@ -272,6 +283,34 @@ CASE( "not_null<>: Allows to construct from a non-null underlying pointer (share
     not_null< shared_ptr< int > > p( pi );
 
     EXPECT( p == pi );
+}
+
+CASE( "not_null<>: Returns underlying pointer with get() (shared_ptr)" )
+{
+    shared_ptr< int > pi = make_shared< int >(12);
+    not_null< shared_ptr< int > > p( pi );
+
+#if gsl_CONFIG( NOT_NULL_TRANSPARENT_GET )
+    int* pg = p.get();
+    EXPECT( pg == pi.get() );
+#else
+    shared_ptr< int > const & pg = p.get();
+    EXPECT( pg == pi );
+#endif
+}
+
+CASE( "not_null<>: Allows to move from a not_null pointer (shared_ptr)" )
+{
+#if gsl_HAVE( FUNCTION_REF_QUALIFIER )
+    shared_ptr< int > pi = make_shared< int >(12);
+    int* raw(pi.get());
+
+    not_null< shared_ptr< int > > p ( std::move(pi) ); // There...
+    pi = std::move(p); // ...and back again.
+
+    EXPECT_THROWS( *p );
+    EXPECT( pi.get() == raw );
+#endif
 }
 
 CASE( "not_null<>: Allows to construct from a non-null underlying pointer (shared_ptr) with make_not_null()" )
@@ -443,7 +482,25 @@ CASE( "not_null<>: Allows to construct from a non-null underlying pointer (uniqu
     EXPECT( &*p == raw );
 }
 
-CASE( "not_null<>: Allows to move from a not-null pointer (unique_ptr)" )
+CASE( "not_null<>: Returns underlying pointer with get() (unique_ptr)" )
+{
+    unique_ptr< int > pi = make_unique< int >(12);
+    int* raw(pi.get());
+    not_null< unique_ptr< int > > p( std::move(pi) );
+
+#if gsl_CONFIG( NOT_NULL_TRANSPARENT_GET )
+    int* pg = p.get();
+    EXPECT( pg == raw );
+#elif gsl_CONFIG( NOT_NULL_GET_BY_CONST_REF )
+    int* pg = p.get().get();
+    EXPECT( pg == raw );
+#else
+    (void) pi;
+    (void) p;
+#endif
+}
+
+CASE( "not_null<>: Allows to move from a not_null pointer (unique_ptr)" )
 {
 #if gsl_HAVE( FUNCTION_REF_QUALIFIER )
     unique_ptr< int > pi = make_unique< int >(12);